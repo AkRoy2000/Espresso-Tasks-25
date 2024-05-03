2b44698132be7f8d114563f9271fc58718ac2c76
abhik <abhikroy@Abhiks-MacBook-Pro.local>
2024-05-03 22:41:57 +0100

    first commit

--- middleware.ts
+++ middleware.ts
@@ -8,11 +8,3 @@ export const config = {
   matcher: [ '/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 };
 
-/*import { authMiddleware } from "@clerk/nextjs/server";
- 
-export default authMiddleware({});
- 
-export const config = {
-  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
-};
-*/
