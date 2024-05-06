fdf242e53eb3e5c86661677f90a1022a3c30e8ff
abhik <abhikroy@Abhiks-MacBook-Pro.local>
2024-05-06 02:49:06 +0100

    first commit

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,6 @@ import { authMiddleware } from "@clerk/nextjs/server";
 
 // Use Clerk authentication middleware to protect routes
 export default authMiddleware({
-  publicRoutes: ["/"]
 });
 
 // Configure routes that will be protected by the authentication middleware
